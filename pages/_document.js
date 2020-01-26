e149fec41b8e48875c2e604f6e72da702e938cae
JulianoV <ventolajuliano@gmail.com>
2020-01-26 18:16:53 -0300

    Style using styled-components working without js

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,25 @@
 {
   /* Need to reload NEXT */
 }
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <html>
         {/* Any Header from other pages is append to this */}
-        <Head>
-          <style>{`body {background: #069}`}</style>
-        </Head>
+        <Head>{this.props.styleTags}</Head>
         <body>
           {/* Content of pages rendered */}
           <Main />
--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Link from 'next/link';
 import Head from 'next/head';
 
 import WithAnalytics from '../src/hocs/withAnalytics';
+
+const Title = styled.h1`
+  color: #069;
+  font-size: 40px;
+`;
 
 const Home = () => (
   <div>
@@ -10,7 +17,7 @@ const Home = () => (
       <title>Home</title>
     </Head>
     <img src='/static/dog.jpg' alt='Dog' width='200' />
-    <h1>Hello World</h1>
+    <Title>Hello World</Title>
     <Link href='/users'>
       <a>Usuários</a>
     </Link>
